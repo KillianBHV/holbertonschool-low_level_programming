71c22650f719198315af43ef627fa62bd1066fdd
Killian BEHAVA <behavakillian70@gmail.com>
2025-07-25 08:38:23 +0000

    Get the length of a specified string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "2-main.h"
+#include <unistd.h>
+
+/**
+ * _strlen - get the length of a string
+ * @s: string to compute
+ *
+ * Return: length of selected string
+ */
+int _strlen(char *s)
+{
+	return strlen(s);
+}
