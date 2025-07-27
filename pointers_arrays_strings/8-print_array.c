9c22f978ead6177166c97910e032deffcc9506a7
Killian BEHAVA <behavakillian70@gmail.com>
2025-07-27 13:58:40 +0200

    Print the elements of an array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array - print the elements of an array
+ * @a: pointer to an array
+ * @b: size of the pointed array
+ *
+ * Return: Nothing
+ */
+void print_array(int *a, int b)
+{
+	for(int i = 0; i < b; i++)
+	{
+		printf("%d", *(a+i));
+		if (i < (b - 1))
+		{
+			printf(", ");	
+		}
+	}
+	printf("\n");
+}
