8a37b2a8f2a5eb2509d7bf88a1aca1d5275bf061
Killian BEHAVA <behavakillian70@gmail.com>
2025-07-27 13:06:37 +0200

    3- Prints a reverse string

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print-rev - Print the reverse selected string
+ * @s: String to reverse
+ *
+ * Return: Nothing
+ */
+void print_rev(char *s)
+{
+	char *p;
+
+	p = s;
+	while (*p != '\0')
+	{
+		p += 1;
+	}
+
+	while (p >= s)
+	{
+		_putchar(*p);
+		p -= 1;
+	}
+	_putchar('\n');
+}
